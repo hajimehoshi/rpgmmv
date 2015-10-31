1d19cb12e6504bf9518198696b6195382e67ccf3
Hajime Hoshi <hajimehoshi@gmail.com>
2015-11-01 02:58:39 +0900

    Add SaveRestriction plugin

---
+++ HajimeHoshi_SaveRestriction.js
@@ -0,0 +1,34 @@
+// Copyright 2015 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*:
+ * @plugindesc Save restriction
+ * @author Hajime Hoshi
+ *
+ * @help This plugin forbids 'save' command in maps those have '<no_save>' meta data.
+ */
+
+(function() {
+    var parameters = PluginManager.parameters('HajimeHoshi_SaveRestriction');
+    var tilesetIds = String(parameters['Tileset IDs'] || '4');
+    tilesetIds = tilesetIds.split(',').map(function(id) { return parseInt(id, 10); });
+
+    var _Window_MenuCommand_isSaveEnabled = Window_MenuCommand.prototype.isSaveEnabled;
+    Window_MenuCommand.prototype.isSaveEnabled = function() {
+        if (!_Window_MenuCommand_isSaveEnabled.call(this)) {
+            return false;
+        }
+        return !$dataMap.meta['no_save'];
+    };
+})();
