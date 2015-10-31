78dbf84a5006f3c55b1027a2c193768d403186a5
Hajime Hoshi <hajimehoshi@gmail.com>
2015-11-01 03:17:08 +0900

    SaveRestriction: Add 'default value' param

--- HajimeHoshi_SaveRestriction.js
+++ HajimeHoshi_SaveRestriction.js
@@ -16,19 +16,30 @@
  * @plugindesc Save restriction
  * @author Hajime Hoshi
  *
- * @help This plugin forbids 'save' command in maps those have '<no_save>' meta data.
+ * @param Default Value
+ * @desc 'Save' is forbidden by default if this value is 0. Otherwise 'save' is admitted by default.
+ * @default 1
+ *
+ * @help This plugin forbids 'save' command in maps those have '<save_disabled>' meta data
+ * and admits maps with <save_enabled>.
+ * If a map doesn't have neither of meta data, it follows the default value.
  */
 
 (function() {
     var parameters = PluginManager.parameters('HajimeHoshi_SaveRestriction');
-    var tilesetIds = String(parameters['Tileset IDs'] || '4');
-    tilesetIds = tilesetIds.split(',').map(function(id) { return parseInt(id, 10); });
+    var defaultValue = true;
+    if (parameters['Default Value'] !== undefined) {
+        defaultValue = Number(parameters['Default Value']) !== 0;
+    }
 
     var _Window_MenuCommand_isSaveEnabled = Window_MenuCommand.prototype.isSaveEnabled;
     Window_MenuCommand.prototype.isSaveEnabled = function() {
         if (!_Window_MenuCommand_isSaveEnabled.call(this)) {
             return false;
         }
-        return !$dataMap.meta['no_save'];
+        if (!defaultValue) {
+            return 'save_enabled' in $dataMap.meta;
+        }
+        return !('save_disabled' in $dataMap.meta);
     };
 })();
