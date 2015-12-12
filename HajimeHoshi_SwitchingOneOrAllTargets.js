74030d2abb0d6211f72ade2af87734760d32b3b1
Hajime Hoshi <hajimehoshi@gmail.com>
2015-12-12 18:22:33 +0900

    Add SwitchingOneOrAllTargets

---
+++ HajimeHoshi_SwitchingOneOrAllTargets.js
@@ -0,0 +1,99 @@
+// Copyright 2015 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*:
+ * @plugindesc Switching one or all targets
+ * @author Hajime Hoshi
+ *
+ * @help This plugin enables to switch one or all targets.
+ */
+
+(function() {
+    'use strict';
+
+    // TODO: This value is already used at forceAction. Is this OK?
+    var TARGET_ALL = -2;
+
+    var ENEMY_ALL = new Object();
+
+    // single/all
+    var _Window_BattleEnemy_maxItems = Window_BattleEnemy.prototype.maxItems;
+    Window_BattleEnemy.prototype.maxItems = function() {
+        // TODO: Add a meta value not to select all enemies.
+        var maxItems = _Window_BattleEnemy_maxItems.call(this);
+        if (maxItems === 1) {
+            return 1;
+        }
+        return maxItems + 1;
+    };
+
+    var _Window_BattleEnemy_enemy = Window_BattleEnemy.prototype.enemy;
+    Window_BattleEnemy.prototype.enemy = function() {
+        var enemy = _Window_BattleEnemy_enemy.call(this);
+        if (enemy) {
+            return enemy;
+        }
+        if (this.index() === this._enemies.length) {
+            return ENEMY_ALL;
+        }
+        return null;
+    };
+
+    var _Window_BattleEnemy_enemyIndex = Window_BattleEnemy.prototype.enemyIndex;
+    Window_BattleEnemy.prototype.enemyIndex = function() {
+        if (this.index() === this._enemies.length) {
+            return TARGET_ALL;
+        }
+        return _Window_BattleEnemy_enemyIndex.call(this);
+    };
+
+    var _Window_BattleEnemy_drawItem = Window_BattleEnemy.prototype.drawItem;
+    Window_BattleEnemy.prototype.drawItem = function(index) {
+        if (index === this._enemies.length) {
+            this.resetTextColor();
+            var name = '(All)';
+            var rect = this.itemRectForText(index);
+            this.drawText(name, rect.x, rect.y, rect.width);
+            return;
+        }
+        _Window_BattleEnemy_drawItem.call(this, index);
+    };
+
+    var _Game_Action_targetsForOpponents = Game_Action.prototype.targetsForOpponents;
+    Game_Action.prototype.targetsForOpponents = function() {
+        if (this._targetIndex === TARGET_ALL) {
+            return this.opponentsUnit().aliveMembers();
+        }
+        return _Game_Action_targetsForOpponents.call(this);
+    };
+
+    var _Game_Action_targetsForFriends = Game_Action.prototype.targetsForFriends;
+    Game_Action.prototype.targetsForFriends = function() {
+        if (this._targetIndex === TARGET_ALL) {
+            return this.friendsUnit().aliveMembers();
+        }
+        return _Game_Action_targetsForFriends.call(this);
+    };
+
+    var _Game_Unit_select = Game_Unit.prototype.select;
+    Game_Unit.prototype.select = function(activeMember) {
+        if (activeMember === ENEMY_ALL) {
+            this.members().forEach(function(member) {
+                member.select();
+            });
+            return;
+        }
+        _Game_Unit_select.call(this, activeMember);
+    };
+})();
