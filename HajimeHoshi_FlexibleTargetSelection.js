bde4a6eddb4f74f920b53f34f65125f79e60cfbd
Hajime Hoshi <hajimehoshi@gmail.com>
2016-01-13 02:00:42 +0900

    FlexibleSelection: Bug fix: tp should be an integer

--- HajimeHoshi_FlexibleTargetSelection.js
+++ HajimeHoshi_FlexibleTargetSelection.js
@@ -224,8 +224,8 @@
     var _Game_Action_applyItemUserEffect = Game_Action.prototype.applyItemUserEffect;
     Game_Action.prototype.applyItemUserEffect = function(target) {
         if (this._isSelectionEnlarged) {
-            var value = Math.floor(this.item().tpGain * this.subject().tcr);
-            this.subject().gainSilentTp(value / 2);
+            var value = this.item().tpGain * this.subject().tcr;
+            this.subject().gainSilentTp(Math.floor(value / 2));
             return;
         }
         _Game_Action_applyItemUserEffect.call(this,target);
